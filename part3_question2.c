735a86c97bd6e20d88f031e772d0e5f711d3c427
aishik124 <164086213+aishik124@users.noreply.github.com>
2024-04-04 17:14:39 -0400

    part3_question2.c

--- part3_question2.c
+++ part3_question2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 11
 
+int main()
+{
+	printf ("s1: ");
+		scanf ("%s", s1); getchar();
+		
+		printf ("s2: ");
+		scanf ("%s", s2); getchar();
+		
+	for(i=0;i<strlen(s1);i++)
+	{
+		for(j=0;j<strlen(s2);j++)	
+		{
+			if(str)
+		}
+	}
+}
