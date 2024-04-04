a98cada716e70ca4286d8f1e1f8d198b99395d60
aishik124 <164086213+aishik124@users.noreply.github.com>
2024-04-04 16:36:02 -0400

    part3_question1.c

--- part3_question1.c
+++ part3_question1.c
@@ -1,1 +1,29 @@
+//Aishik Barua ID:115899946
+#include <stdio.h>
+#include <string.h>
+#define MAX 11
+
+void shift(char str[]) {
+    int length = strlen(str);
+    for (int i = 0; i <=length; i--) {
+        str[i] = str[i + 1];
+    }
+    
+}
+
+int main() {
+    char str[MAX];
+
+    printf("Enter a string of 10 characters: ");
+    scanf("%s", str);
+
+    printf("%s\n", str);
+
+    for (int i = 0; i < 5; i++) {
+        shift(str);
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
 
