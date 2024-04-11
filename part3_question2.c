7a78e614c7daf8b365facd0f448deb7e058bbb26
aishik124 <164086213+aishik124@users.noreply.github.com>
2024-04-11 13:39:20 -0400

    part3_question2.c

--- part3_question2.c
+++ part3_question2.c
@@ -1,20 +1,37 @@
+//Aishik Barua ID:1158999946
 #include <stdio.h>
 #include <string.h>
-#define MAX 11
+#define MAX 100
 
-int main()
+void commonletters(char str1[], char str2[]) 
 {
-	printf ("s1: ");
-		scanf ("%s", s1); getchar();
-		
-		printf ("s2: ");
-		scanf ("%s", s2); getchar();
-		
-	for(i=0;i<strlen(s1);i++)
+    int i, j;
+    for (i = 0; i < strlen(str2); i++) 
 	{
-		for(j=0;j<strlen(s2);j++)	
+        for (j = 0; j < strlen(str1); j++) 
 		{
-			if(str)
-		}
-	}
+            if (str1[j] == str2[i]) 
+			{
+                strcpy(&str1[j], &str1[j + 1]); 
+            }
+        }
+    }
+}
+
+int main() 
+{
+    char str1[MAX], str2[MAX];
+    
+    printf("Enter first string: ");
+    fgets(str1, MAX, stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, MAX, stdin);
+
+    commonletters(str1, str2);
+
+    printf("\nOutput:");
+    printf("%s", str1);
+
+    return 0;
 }
